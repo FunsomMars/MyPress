2f9b6e7b8012e339329ba63952757103f73b3075
Mars <mars@example.com>
2026-03-20 13:49:17 +0800

    添加用户组名称中文转换过滤器

--- templatetags/custom_filters.py
+++ templatetags/custom_filters.py
@@ -6,3 +6,13 @@ register = template.Library()
 def has_group(user, group_name):
     """检查用户是否属于某个用户组"""
     return user.groups.filter(name=group_name).exists()
+
+@register.filter
+def group_name_cn(group_name):
+    """将英文用户组名转换为中文"""
+    group_map = {
+        'Editors': '编辑',
+        'Moderators': '版主',
+        'Administrators': '管理员'
+    }
+    return group_map.get(group_name, group_name)
